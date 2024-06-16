591a282eeba65546c9d8f03162dd032ccafb2c77
Aleksandr <aleksandr.voitushenko@gmail.com>
2024-06-16 22:17:33 +0200

    setup.py and __init__.py - files to install helper-app checkout to test_branch

--- modul_2(Poetry+Docker)/docker_test/setup.py
+++ modul_2(Poetry+Docker)/docker_test/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='helper-app',
       version='1',
       description='Very useful code',
-      url='http://github.com/dummy_user/useful',
+      url='https://github.com/aleksVoit/Python_web/tree/master/modul_2(Poetry%2BDocker)/docker_test',
       author='Flying Circus',
       author_email='flyingcircus@example.com',
       license='MIT',
