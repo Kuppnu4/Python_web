f28cc7aef5aeeca420fe79285039090a03bb6c49
Aleksandr <aleksandr.voitushenko@gmail.com>
2024-06-16 22:07:30 +0200

    setup.py and __init__.py - files to install helper-app

---
+++ modul_2(Poetry+Docker)/docker_test/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='helper-app',
+      version='1',
+      description='Very useful code',
+      url='http://github.com/dummy_user/useful',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['useful'])
